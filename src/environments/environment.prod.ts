689d927c3e388c4c20e7a40b6a47836979aa55b2
Abhishek Kumar <abhishek.omex@gmail.com>
2021-11-08 10:55:32 +0530

    added code config url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  url: 'https://omex-backend.vercel.app/api/v1'
-};
+  url: 'om-agency-bk-jo975zr6o-abhi1272.vercel.app/api/v1'
+  // url: 'http://localhost:4000/api/v1'
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  // url: 'https://omex-backend.vercel.app/api/v1'
-  url: 'http://localhost:4000/api/v1'
-};
+  url: 'om-agency-bk-jo975zr6o-abhi1272.vercel.app/api/v1'
+  // url: 'http://localhost:4000/api/v1'
+}
 
 
 /*
