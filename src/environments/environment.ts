4a6a298c1f0e221e15b30dcd4676a31244574497
Abhishek Kumar <abhishek.omex@gmail.com>
2021-11-08 11:14:58 +0530

    correct env var

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  url: 'om-agency-bk-jo975zr6o-abhi1272.vercel.app/api/v1'
+  url: 'https://om-agency-bk-jo975zr6o-abhi1272.vercel.app/api/v1'
   // url: 'http://localhost:4000/api/v1'
-}
+};
 
 
 /*
